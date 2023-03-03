2b2ddd96977c92738d77ef9bb97fc64916e4d552
Danil <mydogfromhell666@gmail.com>
2023-03-03 13:46:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "./ui/Button/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='box'>
+          <Button text="Label" small background="gray" />
+          <Button text="Label" small background="red" />
+          <Button text="Label" small background="green" />
+          <Button text="Label" small background="orange" />
+          <Button text="Label" small border='green' />
+          <Button text="Label" small border='red' />
+          <Button text="Label" small border='gray' />
+          <Button text="Label" small border='orange' />
+      </div>
+
+      <div className='box'>
+          <Button text="Label" medium background="gray" />
+          <Button text="Label" medium background="red" />
+          <Button text="Label" medium background="green" />
+          <Button text="Label" medium background="orange" />
+          <Button text="Label" medium border='green' />
+          <Button text="Label" medium border='red' />
+          <Button text="Label" medium border='gray' />
+          <Button text="Label" medium border='orange' />
+      </div>
+
+      <div className='box'>
+          <Button text="Label" large background="gray" />
+          <Button text="Label" large background="red" />
+          <Button text="Label" large background="green" />
+          <Button text="Label" large background="orange" />
+          <Button text="Label" large border='green' />
+          <Button text="Label" large border='red' />
+          <Button text="Label" large border='gray' />
+          <Button text="Label" large border='orange' />
+      </div>
+
+    </>
   );
 }
 
